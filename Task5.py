91f735c8d756ab860e5419fff8017f685965228c
bubalush@gmail.com <english>
2017-07-19 23:45:17 +0300

    json edit

--- Task5.py
+++ Task5.py
@@ -1,7 +1,7 @@
 import configparser
 import json
 import psutil
-import datetime
+from datetime import datetime
 import time
 import os
 
@@ -9,7 +9,6 @@ config = configparser.ConfigParser()
 config.read('conf.ini')
 a=config.get('common', 'interval')
 b=config.get('common', 'output')
-print (a,b)
 i=1
 
 
@@ -20,14 +19,32 @@ while True:
     iocounter = str(psutil.disk_io_counters(perdisk=False).busy_time // 100)+'sec'
     netcounter = str(psutil.net_io_counters().bytes_recv // 1024 // 1024)+'MB'
     if b == 'json':
-        with open('output.json', 'w') as outfile:
-            json.dump(network, outfile)
+        num="SNAPSHOT {}".format(i)
+        jsondict={num:{'TIMESTAMP': datetime.now().strftime("%d-%m-%Y %H:%M:%S"),'CPU load': cpu, 'Memory': virtmem,'VirtMem': swap,'IO': iocounter,'Network': netcounter}}
+        if os.path.isfile('output.json') is True:
+            if os.stat('output.json').st_size ==0:
+                with open('output.json', 'w') as outfile:
+                    json.dump(jsondict, outfile, indent=4)
+            else:
+                f=open('output.json', 'r+')
+                dictforjson=json.load(f)
+                g=1
+                for x in dictforjson.keys():
+                    if int(x.split(" ")[1]) > g:
+                        g=int(x.split(" ")[1])
+                i=g+1
+                num="SNAPSHOT {}".format(i)
+                dictforjson[num]={'TIMESTAMP': datetime.now().strftime("%d-%m-%Y %H:%M:%S"),'CPU load': cpu, 'Memory': virtmem,'VirtMem': swap,'IO': iocounter, 'Network': netcounter}
+ +              f.seek(0, 0)
+ +              json.dump(dictforjson, f, indent=4)
+ +              f.close()
+
     elif b == 'txt':
         f=open('output.txt','a+')
         if os.stat('output.txt').st_size > 0:
             f.seek(0, 0)
             i = int(f.readlines()[-1].split(':')[0].split(' ')[1]) + 1
-        pop='SNAPSHOT '+str(i)+': ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        pop='SNAPSHOT '+str(i)+': ' + datetime.now().strftime("%d-%m-%Y %H:%M:%S")
         pop+=': CPU load '+cpu
         pop+= ': Memory '+virtmem
         pop+= ': VirtMem '+swap
@@ -37,7 +54,7 @@ while True:
         f.close()
 
     else:
-        print('Specify json or txt format output in conf.ini')
+        print("Specify 'json' or 'txt' format output in conf.ini")
         break
 
     time.sleep(1)
