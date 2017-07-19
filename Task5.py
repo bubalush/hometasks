36cc1430dd7b0451189298aab0bd719ba56ba043
Nikita Dolya <nikita_dolya@epam.com>
2017-07-19 19:46:32 +0300

    psutil

--- Task5.py
+++ Task5.py
@@ -12,7 +12,13 @@ b=config.get('common', 'output')
 print (a,b)
 i=1
 
+
 while True:
+    cpu = str(psutil.cpu_percent(interval=1))
+    virtmem = str(psutil.virtual_memory().used // 1024 // 1024)+'MB'
+    swap = str(psutil.swap_memory().used // 1024 // 1024)+'MB'
+    iocounter = str(psutil.disk_io_counters(perdisk=False).busy_time // 100)+'sec'
+    netcounter = str(psutil.net_io_counters().bytes_recv // 1024 // 1024)+'MB'
     if b == 'json':
         with open('output.json', 'w') as outfile:
             json.dump(network, outfile)
@@ -21,12 +27,12 @@ while True:
         if os.stat('output.txt').st_size > 0:
             f.seek(0, 0)
             i = int(f.readlines()[-1].split(':')[0].split(' ')[1]) + 1
-        pop='SNAPSHOT '+str(i)+' :' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")+':'
-        pop+='| CPU load '+str(psutil.cpu_percent(interval=1))
-        pop+= '| Memory '+str(psutil.virtual_memory().used//1024//1024)+'MB'
-        pop+= '| VirtMem '+str(psutil.swap_memory().used//1024//1024)+'MB'
-        pop+= '| IO '+str(psutil.disk_io_counters(perdisk=False).busy_time//100)+'sec'
-        pop+= '| Network '+str(psutil.net_io_counters().bytes_recv//1024//1024)+'MB'+'\n'
+        pop='SNAPSHOT '+str(i)+': ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        pop+=': CPU load '+cpu
+        pop+= ': Memory '+virtmem
+        pop+= ': VirtMem '+swap
+        pop+= ': IO '+iocounter
+        pop+= ': Network '+netcounter+'\n'
         f.write(pop)
         f.close()
 
