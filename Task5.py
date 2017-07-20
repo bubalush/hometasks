11202492f959c33b894d60a6a1ebd18d254f5cd0
Nikita Dolya <nikita_dolya@epam.com>
2017-07-20 10:11:53 +0300

    json edit

--- Task5.py
+++ Task5.py
@@ -7,7 +7,7 @@ import os
 
 config = configparser.ConfigParser()
 config.read('conf.ini')
-a=config.get('common', 'interval')
+a=int(config.get('common', 'interval'))*60
 b=config.get('common', 'output')
 i=1
 
@@ -19,26 +19,30 @@ while True:
     iocounter = str(psutil.disk_io_counters(perdisk=False).busy_time // 100)+'sec'
     netcounter = str(psutil.net_io_counters().bytes_recv // 1024 // 1024)+'MB'
     if b == 'json':
-        num="SNAPSHOT {}".format(i)
-        jsondict={num:{'TIMESTAMP': datetime.now().strftime("%d-%m-%Y %H:%M:%S"),'CPU load': cpu, 'Memory': virtmem,'VirtMem': swap,'IO': iocounter,'Network': netcounter}}
-        if os.path.isfile('output.json') is True:
-            if os.stat('output.json').st_size ==0:
-                with open('output.json', 'w') as outfile:
-                    json.dump(jsondict, outfile, indent=4)
-            else:
-                f=open('output.json', 'r+')
-                dictforjson=json.load(f)
-                g=1
-                for x in dictforjson.keys():
-                    if int(x.split(" ")[1]) > g:
-                        g=int(x.split(" ")[1])
-                i=g+1
-                num="SNAPSHOT {}".format(i)
-                dictforjson[num]={'TIMESTAMP': datetime.now().strftime("%d-%m-%Y %H:%M:%S"),'CPU load': cpu, 'Memory': virtmem,'VirtMem': swap,'IO': iocounter, 'Network': netcounter}
- +              f.seek(0, 0)
- +              json.dump(dictforjson, f, indent=4)
- +              f.close()
-
+        try:
+            f = open("output.json", 'a+')
+            f.seek(0, 0)
+            info=json.load(f)
+            num=len(info["LOG"])
+            f.close()
+            f=open("output.json", 'w')
+            print("Appending")
+        except json.decoder.JSONDecodeError:
+            print("New")
+            info={}
+            info["LOG"]=[]
+            num=0
+        write={}
+        write['SNAPSHOT']=num+1
+        write['TIMESTAMP']=datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        write['CPU load']=cpu
+        write['Memory']=virtmem
+        write['VirtMem']=swap
+        write['IO']=iocounter
+        write['Network Received']=netcounter
+        info["LOG"].append(write)
+        json.dump(info, f, indent=4)
+        f.close()
     elif b == 'txt':
         f=open('output.txt','a+')
         if os.stat('output.txt').st_size > 0:
@@ -49,7 +53,7 @@ while True:
         pop+= ': Memory '+virtmem
         pop+= ': VirtMem '+swap
         pop+= ': IO '+iocounter
-        pop+= ': Network '+netcounter+'\n'
+        pop+= ': Network Received' +netcounter+'\n'
         f.write(pop)
         f.close()
 
@@ -57,6 +61,6 @@ while True:
         print("Specify 'json' or 'txt' format output in conf.ini")
         break
 
-    time.sleep(1)
+    time.sleep(a)
 
 
